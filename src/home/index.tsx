4a79036aeb3d483bba790a024f0026c28c82cc06
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-08 03:28:53 +0800

    Revert adding webp variants to images
    This reverts commit 049be032a50bd8945bba2bc428dc9896cb70e46c.

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -5,11 +5,6 @@ import bg from "../assets/bg.jpg";
 import bg2x from "../assets/bg-2x.jpg";
 import bg4x from "../assets/bg-4x.jpg";
 
-import bgSmWebp from "../assets/bg-sm.webp";
-import bgWebp from "../assets/bg.webp";
-import bg2xWebp from "../assets/bg-2x.webp";
-import bg4xWebp from "../assets/bg-4x.webp";
-
 const Home = () => {
   const handleInit = (typewriter: TypewriterClass) => {
     typewriter
@@ -23,17 +18,10 @@ const Home = () => {
     <div class="home-container">
       <div class="relative">
         <picture class="absolute top-0 left-0 -z-10 overflow-hidden background-image">
-          <source srcset={bg4xWebp} media="(min-width: 2000px)" type="image/webp" />
-          <source srcset={bg4x} media="(min-width: 2000px)" type="image/jpeg" />
-
-          <source srcset={bg2xWebp} media="(min-width: 1100px)" type="image/webp" />
-          <source srcset={bg2x} media="(min-width: 1100px)" type="image/jpeg" />
-
-          <source srcset={bgWebp} media="(min-width: 900px)" type="image/webp" />
-          <source srcset={bg} media="(min-width: 900px)" type="image/jpeg" />
-
-          <img src={bgSmWebp} alt="background" type="image/webp" />
-          <img src={bgSm} alt="background" type="image/jpeg" />
+          <source srcset={bg4x} media="(min-width: 2000px)" />
+          <source srcset={bg2x} media="(min-width: 1100px)" />
+          <source srcset={bg} media="(min-width: 900px)" />
+          <img src={bgSm} alt="" />
         </picture>
 
         <div class="flex text-4xl flex justify-center items-center welcome-message">
