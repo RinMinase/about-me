44c75e4e4ed2e9d3d6253c7a1e280eda543590ed
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-08 02:22:11 +0800

    Added routes

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { h } from "preact";
+import { Router, Route } from "preact-router";
+
+import Home from "./home";
+
+const Routes = () => (
+  <Router>
+    <Route path="/" component={Home} />
+
+    <Route default component={Home} />
+  </Router>
+);
+
+export default Routes;
