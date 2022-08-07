3153e5f203795357a5977c68215b87160fea3d9a
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-08 02:20:18 +0800

    Fixed esbuild warnings

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
 })
