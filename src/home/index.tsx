d467025ffe6d56ac11844b32780655ddfbe5daa2
Rin Minase <rin.black.raison.detre@gmail.com>
2022-08-08 02:42:20 +0800

    Added responsive image

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,5 +1,9 @@
 import Typewriter, { TypewriterClass } from "typewriter-effect";
 
+import bg from "../assets/bg.jpg";
+import bg2x from "../assets/bg-2x.jpg";
+import bg4x from "../assets/bg-4x.jpg";
+
 const Home = () => {
   const handleInit = (typewriter: TypewriterClass) => {
     typewriter
@@ -10,8 +14,16 @@ const Home = () => {
   };
 
   return (
-    <div class="text-4xl">
-      <Typewriter onInit={handleInit} options={{ cursor: "_", delay: 75 }} />
+    <div>
+      <picture>
+        <source srcset={bg4x} media="(min-width: 2000px)" />
+        <source srcset={bg2x} media="(min-width: 1100px)" />
+        <img src={bg} alt="" />
+      </picture>
+
+      <div class="text-4xl">
+        <Typewriter onInit={handleInit} options={{ cursor: "_", delay: 75 }} />
+      </div>
     </div>
   );
 };
